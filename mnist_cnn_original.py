34d4607db0397a0067f0ea8c42a4c264fd4c7c7d
임소정 <xithachachki@naver.com>
2025-04-16 22:26:00 +0900

    practicing!!!!

--- mnist_cnn_original.py
+++ mnist_cnn_original.py
@@ -1,7 +1,7 @@
 '''Trains a simple convnet on the MNIST dataset.
 Gets to 99.25% test accuracy after 12 epoch,hahahah
 (there is still a lot of margin for parameter tuning).
-16 seconds per epoch on a GRID K520 GPU.
+16 seconds per epoch on a GRID K520 GPU. Hello my name is bobe
 '''
 
 from __future__ import print_function
